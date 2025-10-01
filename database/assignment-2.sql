32f87036c9cf4ba181f7e2c67d1432f46dd94357
El-Benson <122732891+El-Benson@users.noreply.github.com>
2025-10-01 23:36:37 +0200

    Create assignment-2.sql

---
+++ database/assignment-2.sql
@@ -0,0 +1,28 @@
+-- 1. Insert Tony Stark into the account table
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2. Update Tony Stark to become an Admin
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- 3. Delete Tony Stark from the account table
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+-- 4. Update GM Hummer description
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5. Select inventory + classification (Sport only)
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+-- 6. Update image paths to include /vehicles
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
