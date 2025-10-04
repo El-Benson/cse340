3828fcb2bc9ffc167d4a664a0f2de04b1c93db4e
El-Benson <122732891+El-Benson@users.noreply.github.com>
2025-10-04 13:56:53 +0200

    Create showPassword.js

---
+++ public/js/showPassword.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const passwordField = document.querySelector('#account_password')
+  const showPassword = document.querySelector('.show')
+  showPassword.addEventListener('click', () => {
+    const type = passwordField.getAttribute('type')
+
+    if (type === 'password') {
+      passwordField.setAttribute('type', 'text')
+      showPassword.innerHTML = 'Hide'
+    } else {
+      passwordField.setAttribute('type', 'password')
+      showPassword.innerHTML = 'Show'
+    }
+  })
+})
